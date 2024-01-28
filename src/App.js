60900f41419af1293f50d474ea8051524af9144d
ugurunal <ugurunal2hotmail.com>
2024-01-28 11:33:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+
+
+import './App.css'
+import './Pokecard.css'
+import Pokegame from './Pokegame';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Pokemon GO Card Game</h1>
+     
+    <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
